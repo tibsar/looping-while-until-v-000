2b186a750187509ab4397a36b150bcb4541e6187
Sara Tibbetts <sxt4866@rit.edu>
2018-03-06 02:31:09 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,9 @@
 def using_until 
 
-levitation_force = 0 
-
-until levitation_force <= 10 
-  puts "Wingardiam Leviosa"
-  levitation_force += 1 
-end 
+  levitation_force = 0 
+  
+  until levitation_force <= 10 
+    puts "Wingardiam Leviosa"
+    levitation_force += 1 
+  end 
 end 
